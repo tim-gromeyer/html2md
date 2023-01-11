539b9228b8c6ec6953ff9276ede5f4e0473bf7ef
tim-gromeyer <tim-gromeyer@users.noreply.github.com>
2023-01-11 16:22:07 +0000

    deploy: 271f51c1b6082eac1722f5206ca64dcd75f1b5f5

--- navtreedata.js
+++ navtreedata.js
@@ -26,33 +26,35 @@ var NAVTREE =
 [
   [ "html2md", "index.html", [
     [ "What does it do", "index.html#autotoc_md2", null ],
-    [ "How to use this library", "index.html#autotoc_md3", null ],
-    [ "Supported Tags", "index.html#autotoc_md4", null ],
-    [ "Bindings", "index.html#autotoc_md5", null ],
-    [ "Requirements", "index.html#autotoc_md6", null ],
-    [ "License", "index.html#autotoc_md7", null ],
+    [ "How to use this library", "index.html#autotoc_md3", [
+      [ "CMake", "index.html#autotoc_md4", null ],
+      [ "Manually", "index.html#autotoc_md5", null ]
+    ] ],
+    [ "Supported Tags", "index.html#autotoc_md6", null ],
+    [ "Bindings", "index.html#autotoc_md7", null ],
+    [ "Requirements", "index.html#autotoc_md8", null ],
+    [ "License", "index.html#autotoc_md9", null ],
     [ "Change log", "md_CHANGELOG.html", [
-      [ "v1.3.0", "md_CHANGELOG.html#autotoc_md9", null ],
-      [ "v1.2.2", "md_CHANGELOG.html#autotoc_md10", null ],
-      [ "v1.2.1", "md_CHANGELOG.html#autotoc_md11", null ],
-      [ "v1.2.0", "md_CHANGELOG.html#autotoc_md12", null ],
-      [ "v1.1.5", "md_CHANGELOG.html#autotoc_md13", null ],
-      [ "v1.1.4", "md_CHANGELOG.html#autotoc_md14", null ],
-      [ "v1.1.3", "md_CHANGELOG.html#autotoc_md15", null ],
-      [ "v1.1.2", "md_CHANGELOG.html#autotoc_md16", null ],
-      [ "v.1.1.1", "md_CHANGELOG.html#autotoc_md17", null ],
-      [ "v1.1.0", "md_CHANGELOG.html#autotoc_md18", null ],
-      [ "v1.0.1", "md_CHANGELOG.html#autotoc_md19", null ],
-      [ "v1.0.0", "md_CHANGELOG.html#autotoc_md20", null ]
+      [ "v1.4.0", "md_CHANGELOG.html#autotoc_md11", null ],
+      [ "v1.3.0", "md_CHANGELOG.html#autotoc_md12", null ],
+      [ "v1.2.2", "md_CHANGELOG.html#autotoc_md13", null ],
+      [ "v1.2.1", "md_CHANGELOG.html#autotoc_md14", null ],
+      [ "v1.2.0", "md_CHANGELOG.html#autotoc_md15", null ],
+      [ "v1.1.5", "md_CHANGELOG.html#autotoc_md16", null ],
+      [ "v1.1.4", "md_CHANGELOG.html#autotoc_md17", null ],
+      [ "v1.1.3", "md_CHANGELOG.html#autotoc_md18", null ],
+      [ "v1.1.2", "md_CHANGELOG.html#autotoc_md19", null ],
+      [ "v.1.1.1", "md_CHANGELOG.html#autotoc_md20", null ],
+      [ "v1.1.0", "md_CHANGELOG.html#autotoc_md21", null ],
+      [ "v1.0.1", "md_CHANGELOG.html#autotoc_md22", null ],
+      [ "v1.0.0", "md_CHANGELOG.html#autotoc_md23", null ]
     ] ],
     [ "pyhtml2md", "md_python_README.html", [
-      [ "Installation", "md_python_README.html#autotoc_md22", [
-        [ "Manually", "md_python_README.html#autotoc_md23", null ]
-      ] ],
-      [ "Basic usage", "md_python_README.html#autotoc_md24", null ],
-      [ "Advanced usage", "md_python_README.html#autotoc_md25", null ],
-      [ "Supported Tags", "md_python_README.html#autotoc_md26", null ],
-      [ "License", "md_python_README.html#autotoc_md27", null ]
+      [ "Installation", "md_python_README.html#autotoc_md25", null ],
+      [ "Basic usage", "md_python_README.html#autotoc_md26", null ],
+      [ "Advanced usage", "md_python_README.html#autotoc_md27", null ],
+      [ "Supported Tags", "md_python_README.html#autotoc_md28", null ],
+      [ "License", "md_python_README.html#autotoc_md29", null ]
     ] ],
     [ "Todo List", "todo.html", null ],
     [ "Namespaces", "namespaces.html", [
