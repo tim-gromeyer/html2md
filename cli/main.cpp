fd5b87d35fca96cb32b34b004e454ae8909d328a
Tim Gromeyer <tim.gromeyer@gmail.com>
2023-08-12 00:28:29 +0200

    Rework cli program

--- cli/main.cpp
+++ cli/main.cpp
@@ -16,139 +16,164 @@ using std::ios;
 using std::string;
 using std::stringstream;
 
-namespace file {
+namespace FileUtils {
 bool exists(const std::string &name) {
   ifstream f(name.c_str());
   return f.good();
 }
-} // namespace file
 
-constexpr const char *const description =
-    " [Options]\n\n"
+string readAll(const string &file) {
+  ifstream in(file);
+  stringstream buffer;
+  buffer << in.rdbuf();
+
+  if (in.bad()) {
+    throw std::runtime_error("Error reading file: " + file);
+  }
+
+  return buffer.str();
+}
+
+void writeFile(const string &file, const string &content) {
+  fstream out(file, ios::out);
+  if (!out.is_open()) {
+    throw std::runtime_error("Error writing file: " + file);
+  }
+
+  out << content;
+  out.close();
+
+  if (out.bad()) {
+    throw std::runtime_error("Error writing file: " + file);
+  }
+}
+} // namespace FileUtils
+
+constexpr const char *const DESCRIPTION =
+    " [Options] files...\n\n"
     "Simple and fast HTML to Markdown converter with table support.\n\n"
     "Options:\n"
     "  -h, --help\tDisplays this help information.\n"
     "  -v, --version\tDisplay version information and exit.\n"
     "  -o, --output\tSets the output file.\n"
-    "  -i, --input\tSets the input file or text.\n"
-    "  -p, --print\tPrint Markdown(overrides -o).\n"
+    "  -i, --input\tSets the input text.\n"
+    "  -p, --print\tPrint the generated Markdown.\n"
     "  -r, --replace\tOverwrite the output file (if it already exists) without "
     "asking.\n";
 
-int main(int argc, const char *argv[]) {
-  string input;
-  string inFile;
-  string outFile = "Converted.md";
-
+struct Options {
   bool print = false;
   bool replace = false;
+  string inputFile;
+  string outputFile;
+  string inputText;
+};
+
+void printHelp(const string &programName) {
+  cout << programName << DESCRIPTION;
+}
+
+void printVersion() { cout << "Version " << VERSION << endl; }
+
+bool confirmOverride(const string &fileName) {
+  while (true) {
+    cout << fileName << " already exists, override? [y/n] ";
+    string override;
+    getline(cin, override);
+
+    if (override.empty()) {
+      continue;
+    }
+
+    if (override == "y" || override == "Y") {
+      return true;
+    } else if (override == "n" || override == "N") {
+      return false;
+    } else {
+      cout << "Invalid input" << endl;
+    }
+  }
+}
+
+Options parseCommandLine(int argc, char **argv) {
+  Options options;
 
   if (argc == 1) {
-    cout << argv[0] << description;
-    return 0;
+    printHelp(argv[0]);
+    exit(EXIT_SUCCESS);
   }
 
   for (int i = 1; i < argc; i++) {
-    std::string arg = argv[i];
+    string arg = argv[i];
+
     if (arg == "-h" || arg == "--help") {
-      cout << argv[0] << description; // Show help
-      return 0;
+      printHelp(argv[0]);
+      exit(EXIT_SUCCESS);
     } else if (arg == "-v" || arg == "--version") {
-      cout << "Version " << VERSION << endl;
-      return 0;
+      printVersion();
+      exit(EXIT_SUCCESS);
     } else if (arg == "-p" || arg == "--print") {
-      print = true;
+      options.print = true;
     } else if (arg == "-r" || arg == "--replace") {
-      replace = true;
+      options.replace = true;
     } else if (arg == "-o" || arg == "--output") {
       if (i + 1 < argc) {
-        outFile = argv[i + 1];
+        options.outputFile = argv[i + 1];
         i++;
       } else {
-        std::cerr << "The" << arg
-                  << "option requires a file name!\n'Converted.md' is used."
-                  << std::endl;
+        cerr << "The" << arg << "option requires a file name!\n" << endl;
+        exit(EXIT_FAILURE);
       }
     } else if (arg == "-i" || arg == "--input") {
       if (i + 1 < argc) {
-        inFile = argv[i + 1];
+        options.inputText = argv[i + 1];
         i++;
       } else {
-        std::cerr << "The" << arg << "option requires a filename or HTML text!"
-                  << std::endl;
-        return 1;
+        cerr << "The" << arg << "option requires HTML text!" << endl;
+        exit(EXIT_FAILURE);
       }
+    } else if (options.inputFile.empty()) {
+      options.inputFile = arg;
     }
   }
 
-  if (file::exists(inFile)) {
-    ifstream in(inFile);
-    stringstream buffer;
-    buffer << in.rdbuf();
-    input = std::move(buffer.str());
+  return options;
+}
 
-    if (in.bad()) {
-      cerr << "Error: " << strerror(errno) << ": " << inFile << endl;
-      return 0;
-    }
+int main(int argc, char **argv) {
+  Options options = parseCommandLine(argc, argv);
 
-    in.close();
-  } else
-    input = std::move(inFile);
+  string input;
+  if (!options.inputText.empty()) {
+    input = options.inputText;
+  } else if (!options.inputFile.empty() &&
+             FileUtils::exists(options.inputFile)) {
+    input = FileUtils::readAll(options.inputFile);
+  } else {
+    cerr << "No valid input provided!" << endl;
+    return EXIT_FAILURE;
+  }
 
   html2md::Converter converter(input);
-  auto md = converter.convert();
+  string md = converter.convert();
 
-  if (print) {
+  if (options.print) {
     cout << md << endl;
-    return 0;
   }
 
-  if (file::exists(outFile) && !replace) {
-    string override;
-
-    while (true) {
-      cout << outFile << " already exists, override? [y/n] ";
-      getline(cin, override);
-      if (override.empty())
-        continue;
-
-      stringstream ss(override);
-      string in;
-      ss >> in;
-      if (in.empty() || in.length() > 1) {
-        cout << "Invalid input" << endl;
-        continue;
+  if (!options.outputFile.empty()) {
+    if (FileUtils::exists(options.outputFile) && !options.replace) {
+      if (confirmOverride(options.outputFile)) {
+        FileUtils::writeFile(options.outputFile, md);
+        cout << "Markdown written to " << options.outputFile << endl;
+      } else {
+        cout << "Markdown not written." << endl;
       }
-
-      char c = (char)tolower(in[0]);
-      if (c == 'n')
-        return 0;
-      else if (c == 'y')
-        break;
-      else {
-        cout << "Invalid input" << endl;
-        continue;
-      }
+    } else {
+      FileUtils::writeFile(options.outputFile, md);
+      cout << "Markdown written to " << options.outputFile << endl;
     }
   }
 
-  fstream out;
-  out.open(outFile, ios::out);
-  if (!out.is_open()) {
-    cout << md << endl;
-    return 0;
-  }
-
-  out << md;
-  out.close();
-
-  if (out.bad()) {
-    cerr << "Error: " << strerror(errno) << ": " << outFile << endl;
-    return 1;
-  }
-
-  cout << "Markdown written to " << outFile << endl;
-
-  return 0;
+  return EXIT_SUCCESS;
 }
