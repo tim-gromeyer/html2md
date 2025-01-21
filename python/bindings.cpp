2a0403920ac8012340235d3aa7ab50a8e9a5ad77
Tim Gromeyer <tim.gromeyer@gmail.com>
2025-01-21 08:52:29 +0100

    Complete python bindings

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -5,18 +5,26 @@ namespace py = pybind11;
 PYBIND11_MODULE(pyhtml2md, m) {
   m.doc() = "Python bindings for html2md"; // optional module docstring
 
+  // Options class bindings
   py::class_<html2md::Options>(m, "Options")
       .def(py::init<>())
       .def_readwrite(
           "splitLines", &html2md::Options::splitLines,
           "Add new line when a certain number of characters is reached")
+      .def_readwrite("softBreak", &html2md::Options::softBreak,
+                     "Wrap after ... characters when the next space is reached")
+      .def_readwrite("hardBreak", &html2md::Options::hardBreak,
+                     "Force a break after ... characters in a line")
       .def_readwrite("unorderedList", &html2md::Options::unorderedList,
                      "The char used for unordered lists")
       .def_readwrite("orderedList", &html2md::Options::orderedList,
                      "The char used after the number of the item")
       .def_readwrite("includeTitle", &html2md::Options::includeTitle,
                      "Whether title is added as h1 heading at the very "
-                     "beginning of the markdown");
+                     "beginning of the markdown")
+      .def_readwrite("formatTable", &html2md::Options::formatTable,
+                     "Whether to format Markdown Tables")
+      .def("__eq__", &html2md::Options::operator==);
 
   py::class_<html2md::Converter>(m, "Converter")
       .def(py::init<std::string &, html2md::Options *>(),
